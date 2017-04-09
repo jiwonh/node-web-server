b8ebdaa4e7b864a2b1bdb8d8ff5e9807273a5af7
jiwon <jiwonh@gmail.com>
2017-04-09 11:03:47 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+//app.set('views', 'views');
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+app.use((req, res, next) => {
+  var log = `${new Date().toString()}: ${req.method} ${req.path}\n`
+  fs.appendFile('server.log', log)
+  console.log(log);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to my website."
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    pageTitle: "About Page"
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is running on port 3000')
+});
